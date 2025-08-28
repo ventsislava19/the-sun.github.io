f580a2f504023607616bd7b769f05d8214ca015a
Ventsislava Petrova <69962874+ventsislava19@users.noreply.github.com>
2025-08-28 23:18:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// menu button
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+
+    const visibility = nav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+
+    // console.log(visibility);
+    console.log(navToggle.getAttribute('aria-expanded'));
+
+    }
+)
